e8b371bf5bb6b7fc67aebc2c3ce800c937b5496c
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2015-04-05 20:51:50 +0800

    Added Pi Code

---
+++ pi/char_lcd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi.
+
+import Adafruit_CharLCD as LCD
+
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 25  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 24
+lcd_d4        = 23
+lcd_d5        = 17
+lcd_d6        = 27
+lcd_d7        = 22
+lcd_backlight = 4
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+# Print a two line message
+lcd.message('Welcome to\n5P.IO')
